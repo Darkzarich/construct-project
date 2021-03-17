dbd63c83390299552cc4ab3a41b8902a11de98f7
Darkzarich <darkzarichv2@gmail.com>
2021-03-17 08:10:44 +0300

    configure webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "src", "index"),
-  output: {
-    path: path.join(__dirname, "public"),
-    publicPath: "",
-    filename: "bundle.js",
-    chunkFilename: "[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    inline: true,
-    host: "localhost",
-    watchContentBase: true,
-    open: true,
-    port: 8080,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: path.join(__dirname, "src", "index"),
+    output: {
+      path: path.join(__dirname, "public"),
+      publicPath: "",
+      filename: "bundle.js",
+      chunkFilename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isProduction ? null : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      inline: true,
+      host: "localhost",
+      watchContentBase: true,
+      open: true,
+      port: 8080,
+    },
+  };
 };
