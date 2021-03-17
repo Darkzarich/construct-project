d03438d1a4dbde7da1de5556f2b5450473bec12a
Darkzarich <darkzarichv2@gmail.com>
2021-03-17 09:22:07 +0300

    add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
-  
+  const isProduction = argv.mode === 'production';
+
   return {
-    mode: isProduction ? "production" : "development",
-    entry: path.join(__dirname, "src", "index"),
+    mode: isProduction ? 'production' : 'development',
+    entry: path.join(__dirname, 'src', 'index'),
     output: {
-      path: path.join(__dirname, "public"),
-      publicPath: "",
-      filename: "bundle.js",
-      chunkFilename: "[name].js",
+      path: path.join(__dirname, 'public'),
+      publicPath: '',
+      filename: 'bundle.js',
+      chunkFilename: '[name].js',
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+    ],
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          use: "ts-loader",
+          use: 'ts-loader',
           exclude: /node_modules/,
         },
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
       ],
     },
     resolve: {
-      extensions: [".tsx", ".ts", ".js"],
+      extensions: ['.tsx', '.ts', '.js'],
     },
-    devtool: isProduction ? null : "inline-source-map",
+    devtool: isProduction ? false : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, "public"),
+      contentBase: path.join(__dirname, 'src'),
       inline: true,
-      host: "localhost",
+      host: 'localhost',
       watchContentBase: true,
       open: true,
       port: 8080,
